8994ce9091632170a2bbc758f506a298ef93893a
David Elmer <davelmer@hotmail.com>
2015-02-16 17:42:11 -0500

    Initial angular scaffolding

---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('ngMadLibs', ['ngAnimate', 'ngRoute'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+;
